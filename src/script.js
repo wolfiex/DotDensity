8e4228a734ebc3996aeae6463d9c9f2085621377
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-11-28 16:06:05 +0000

    shader

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
 import maplibregl from 'maplibre-gl';
+//npm i https://github.com/wolfiex/maplibre-gl-js-webgl2
+
 //https://webglfundamentals.org/
+
+//include webgl2 in mapboxgl
+if (
+  maplibregl.Map.prototype._setupPainter.toString ().indexOf ('webgl2') == -1
+) {
+  var _setupPainter_old = maplibregl.Map.prototype._setupPainter;
+  maplibregl.Map.prototype._setupPainter = function () {
+    let getContext_old = this._canvas.getContext;
+    this._canvas.getContext = function (name, attrib) {
+      return (
+        getContext_old.apply (this, ['webgl2', attrib]) ||
+        getContext_old.apply (this, ['webgl', attrib]) ||
+        getContext_old.apply (this, ['experimental-webgl', attrib])
+      );
+    };
+    _setupPainter_old.apply (this);
+    this._canvas.getContext = getContext_old;
+  };
+}
+
 
 var map = (window.map = new maplibregl.Map ({
   container: 'map',
@@ -8,6 +30,8 @@ var map = (window.map = new maplibregl.Map ({
   style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
   antialias: true,
 }));
+document.querySelector ('canvas').style.filter = 'invert(1)';
+
 
 // create a custom style layer to implement the WebGL content
 var highlightLayer = {
@@ -15,36 +39,91 @@ var highlightLayer = {
   type: 'custom',
 
   onAdd: function (map, gl) {
+    console.log (gl.getParameter (gl.VERSION));
     // create GLSL source for vertex shader
-    var vertexSource =
-      '' +
-      'uniform mat4 u_matrix;    ' +
-      'attribute vec2 a_pos;' +
-      'void main() {' +
-      '    gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);' +
-      '}';
+    var vertexSource = `uniform mat4 u_matrix;  
+      attribute vec2 a_pos;
+      void main() {
+          gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);
+      }`;
 
     // create GLSL source for fragment shader
     // need to set precision!!
     var fragmentSource = `
     precision mediump float;
+    precision mediump int;
+    // layout(binding = 0, offset = 0) uniform atomic_uint u_counter;
+    
   // uniform vec2 u_resolution;
   uniform float u_density;
   uniform float u_time;
+  uniform mat4 u_thresholds;
+  
 
-  float rand(vec2 co){
-    return fract(sin(dot(co.xy ,vec2(1.9898,1.2323))) * 43758.5453);
+//   float rand(vec2 co){
+    
+//     // return fract(sin(dot(co.xy ,vec2(1.9898,1.2323))) * 43758.5453);
+//     return noise  (co.xy, 42);
+// }
+
+
+
+
+// Gold Noise ©2015 dcerisano@standard3d.com
+// - based on the Golden Ratio
+// - uniform normalized distribution
+// - fastest static noise generator function (also runs at low precision)
+// - use with indicated fractional seeding method. 
+
+float PHI = 1.61803398874989484820459;  // Φ = Golden Ratio   
+
+float gold_noise(in vec2 co, in float seed){
+       return fract(tan(distance(co.xy*PHI, co.xy)*seed)*co.xy.x);
 }
 
 float show(vec2 co){
-  return float(int(u_density>rand(co)));
+  return 0.7*float(int(u_density>gold_noise(co,42.0)));
 }
 
+
+
   void main() {
+    // uint c = atomicCounterIncrement(ac);
+    // float r = (c/255)/255.f;
     // vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
-    gl_FragColor = vec4(1.0, 0.0,0.0,show(gl_FragCoord.xy));
+
+    // 5 categories
+
+    vec2 co = gl_FragCoord.xy;
+    float thresh = gold_noise(co,42.0);
+
+
+
+
+    if (thresh > u_thresholds[0][3]){
+      gl_FragColor = vec4(u_thresholds[0]);
+    } else if (thresh > u_thresholds[1][3]){
+      gl_FragColor = vec4(u_thresholds[1]);
+    } else if (thresh > u_thresholds[2][3]){
+      gl_FragColor = vec4(u_thresholds[2]);
+    } else if (thresh > u_thresholds[3][3]){
+      gl_FragColor = vec4(u_thresholds[3]);
+    } else {
+      gl_FragColor = vec4(255.,255.,255.,1.);
+    }
+
+
+
+
+
+    // gl_FragColor = ;
+    gl_FragColor[3] = show(co);
   }`;
+
+    //key
+    /// 1 px = x people
+    // 4px y people
 
     console.error (gl);
     // create a vertex shader
@@ -63,9 +142,14 @@ float show(vec2 co){
     gl.attachShader (this.program, fragmentShader);
     gl.linkProgram (this.program);
 
+    
+
     // set the time uniform
-    this.timeLocation = gl.getUniformLocation(this.program, "u_time");
-    this.density = gl.getUniformLocation(this.program, "u_density");
+    this.timeLocation = gl.getUniformLocation (this.program, 'u_time');
+    this.density = gl.getUniformLocation (this.program, 'u_density');
+    this.thresholds = gl.getUniformLocation (this.program, 'u_thresholds');
+
+    window.g = gl;
 
     this.aPos = gl.getAttribLocation (this.program, 'a_pos');
 
@@ -104,9 +188,21 @@ float show(vec2 co){
   // https://maplibre.org/maplibre-gl-js-docs/api/map/#map.event:render
   render: function (gl, matrix) {
     gl.useProgram (this.program);
+    gl.uniform1f (this.timeLocation, 0.5);
+    gl.uniform1f (this.density, 0.29);
+    gl.uniform1f (this.counter, 0);
+    // big to small. //[r,g,b,.gt.]
+    // rgb as a ratio 0-1 i.e. n/255
+    var breaks = [
+      [0.5, 0.5, 0.5, 0.9],
+      [1.1, 0.0, 0.0, 0.45],
+      [0.0, 0.0, 1.2, 0.2],
+      [0.1, 1.1, 0.2, 0.0],
+    ]
+      .flat ()
+      .map (parseFloat);
 
-    gl.uniform1f(this.timeLocation, 0.5);
-    gl.uniform1f(this.density, 0.3);
+    gl.uniformMatrix4fv (this.thresholds, false, breaks);
 
     gl.uniformMatrix4fv (
       gl.getUniformLocation (this.program, 'u_matrix'),
